1f7ab4ba2aa947006f584b66994dc2631c0a46a4
noass <therealfunguyy@gmail.com>
2023-10-09 09:43:40 -0300

    fixed. can now use url public/orders/show/{order_id}

--- app/controllers/orders.php
+++ app/controllers/orders.php
@@ -9,11 +9,15 @@ Class Orders extends Controller{
         $db = new Database();
         $db->connect();
         $orders = $db->read("SELECT o.order_id,
-        CONCAT(c.first_name, ' ', c.last_name) AS customer, o.order_date,
-        os.name AS status, o.comments, o.shipped_date, o.shipper_id 
-        FROM orders o LEFT JOIN order_statuses os 
-        ON o.status = os.order_status_id 
-        LEFT JOIN customers c ON o.customer_id = c.customer_id;")[0];
+        CONCAT(c.first_name, ' ', c.last_name) AS customer,
+        o.order_date,
+        os.name AS status,
+        o.comments,
+        o.shipped_date,
+        o.shipper_id 
+ FROM orders o
+ LEFT JOIN order_statuses os ON o.status = os.order_status_id
+ LEFT JOIN customers c ON o.customer_id = c.customer_id;");
 
         $this->view("orders", ["orders" => $orders]);
     }
@@ -23,11 +27,16 @@ Class Orders extends Controller{
             $db = new Database();
             $db->connect();
             $order = $db->read("SELECT o.order_id,
-            CONCAT(c.first_name, ' ', c.last_name) AS customer, o.order_date,
-            os.name AS status, o.comments, o.shipped_date, o.shipper_id 
-            FROM orders o WHERE order_id=($id) LEFT JOIN order_statuses os 
-            ON o.status = os.order_status_id 
-            LEFT JOIN customers c ON o.customer_id = c.customer_id;")[0];
+            CONCAT(c.first_name, ' ', c.last_name) AS customer,
+            o.order_date,
+            os.name AS status,
+            o.comments,
+            o.shipped_date,
+            o.shipper_id 
+     FROM orders o
+     LEFT JOIN order_statuses os ON o.status = os.order_status_id
+     LEFT JOIN customers c ON o.customer_id = c.customer_id
+     WHERE o.order_id = ($id);")[0];
             //$this->loadModel();
             //$this->view("order", ["order" => $order, "order_inst" => $order_inst]);
             $this->view("order", ["order" => $order]);
--- app/views/order.php
+++ app/views/order.php
@@ -1,15 +1,16 @@
-<h1>Single order:</h1>
+<h1>Single order (by order_id):</h1>
 
 <?php
 echo "<hr>";
-    show($data["order"]);
-    echo "<p>Order ID: " . $data['order_id'] . "</p>";
-    echo "<p>Customer (first name & last name): " . $data['customer'] . "</p>";
-    echo "<p>Order date: " . $data['order_date'] . "</p>";
-    echo "<p>Status: " . $data['status'] . "</p>";
-    echo "<p>Comments: " . $data['comments'] . "</p>";
-    echo "<p>Shipped date: " . $data['shipped_date'] . "</p>";
-    echo "<p>Shipper ID: " . $data['shipper_id'] . "</p>";
+$order = $data["order"];
+    show($order);
+    echo "<p>Order ID: " . $order['order_id'] . "</p>";
+    echo "<p>Customer (first name & last name): " . $order['customer'] . "</p>";
+    echo "<p>Order date: " . $order['order_date'] . "</p>";
+    echo "<p>Status: " . $order['status'] . "</p>";
+    echo "<p>Comments: " . $order['comments'] . "</p>";
+    echo "<p>Shipped date: " . $order['shipped_date'] . "</p>";
+    echo "<p>Shipper ID: " . $order['shipper_id'] . "</p>";
 
     echo "<hr>";
 ?>
